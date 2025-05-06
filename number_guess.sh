0591e3f4d9231fbca5d6dee3828b31dc18f0156b
Subhrangshu Ghosh <ghoshayab@gmail.com>
2025-05-06 07:38:04 +0000

    chore: polish script and trim username to 22 characters

--- number_guess.sh
+++ number_guess.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
+# Number Guessing Game Script
 
-# Connect to the database
 PSQL="psql --username=postgres --dbname=number_guess -t --no-align -c"
 
-# Prompt for username
 echo "Enter your username:"
 read USERNAME
 
-# Check if user exists
+# Trim username if longer than 22 characters
+USERNAME=${USERNAME:0:22}
+
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 
 if [[ -z $USER_ID ]]; then
-  # Insert new user
   INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
   echo "Welcome, $USERNAME! It looks like this is your first time here."
 else
-  # Get stats
   GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
   BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID")
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
-# Generate secret number between 1 and 1000
-SECRET_NUMBER=$((RANDOM % 1000 + 1))
 
-# Prompt to guess
+SECRET_NUMBER=$((RANDOM % 1000 + 1))
 echo "Guess the secret number between 1 and 1000:"
 NUMBER_OF_GUESSES=0
 
 while true; do
   read GUESS
 
-  # Check if input is an integer
-   if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
     echo "That is not an integer, guess again:"
     continue
   fi
@@ -45,7 +41,6 @@ while true; do
   else
     echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
 
-    # Insert game result
     if [[ -z $USER_ID ]]; then
       USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
     fi
